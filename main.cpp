668ded697a0a396c6848652b979d1ec36bd7f0e8
rafay2-source <ar1215729@gmail.com>
2025-06-25 12:25:30 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int subject1, subject2, subject3;
     int total;
     float average;
     char grade;
 
-    // Input marks for 3 subjects
+    
     cout << "Enter marks for subject 1: ";
     cin >> subject1;
     cout << "Enter marks for subject 2: ";
@@ -15,11 +16,11 @@ int main() {
     cout << "Enter marks for subject 3: ";
     cin >> subject3;
 
-    // Calculate total and average
+    
     total = subject1 + subject2 + subject3;
     average = total / 3.0;
 
-    // Determine grade using if/else
+
     if (average >= 80) {
         grade = 'A';
     } else if (average >= 60) {
@@ -27,10 +28,10 @@ int main() {
     } else if (average >= 40) {
         grade = 'C';
     } else {
-        grade = 'F';  // F for Fail
+        grade = 'F';
     }
 
-    // Output results
+    
     cout << "\nTotal Marks = " << total << endl;
     cout << "Average = " << average << endl;
     cout << "Grade = " << grade << endl;
